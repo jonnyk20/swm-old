49a17a4686c5d1d11d51e9494b7ed251089e56aa
Jonathan Kalambay <jonnyk20@gmail.com>
2017-10-05 16:49:24 -0700

    added timer2 class

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const io = require('socket.io')();
 const timer = require('./timer.js');
+const Timer = require('./timer2.js');
 
 // const t = setInterval(() => {
 //   io.broadcast('timer');
 // }, 1000);
 
+const t = new Timer(1);
+
+t.on('tick', (ts) => {
+  console.log('tick', ts);
+  io.emit('tick', {ts});
+})
+
 io.on('connection', (client) => {
   console.log("client connected");
   client.emit('timer', 'welcome!');
+
+  client.on('start', () => t.start());
+  client.on('stop', () => t.stop());
  
   const timerUpdate = function (str) {
     client.emit('timenr', str);
